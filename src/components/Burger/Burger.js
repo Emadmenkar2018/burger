9f346279333db05b9785286810b4987ba06e0988
xotx <ot.themechanic95@gmail.com>
2020-02-09 22:16:14 +0300

    Adjust coming props to output ingredients probably.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,17 +3,28 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => (
+                <BurgerIngredient 
+                    key={ingKey + i}
+                    type={ingKey}
+                />
+            ));
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        console.log(transformedIngredients);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please Start Adding Ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient 
                 type="bread-top"
             />
-            <BurgerIngredient 
-                type="cheese"
-            />
-            <BurgerIngredient 
-                type="meat"
-            />
+            {transformedIngredients}
             <BurgerIngredient 
                 type="bread-bottom"
             />
