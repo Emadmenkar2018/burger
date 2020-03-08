85bb2dd3004db2c0ee19068059aae2850f6d7e6f
xotx <ot.themechanic95@gmail.com>
2020-03-08 19:46:22 +0300

    Add the order now button and its styles.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -36,6 +36,12 @@ const BuildControls = (props) => {
                     disabled={props.disabled[ctrl.type]}
                 />
             )}
+            <button 
+                className={classes.OrderButton}
+                disabled={!props.purchasable}
+            >
+                ORDER NOW
+            </button>
         </div>
     )
 }
