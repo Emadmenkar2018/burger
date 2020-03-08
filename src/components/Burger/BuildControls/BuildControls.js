40cebf4a338390d0e0ac8ef3046ae56efaafa715
xotx <ot.themechanic95@gmail.com>
2020-03-08 20:17:28 +0300

    Add onclick event handler to the order button.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -39,6 +39,7 @@ const BuildControls = (props) => {
             <button 
                 className={classes.OrderButton}
                 disabled={!props.purchasable}
+                onClick={props.ordered}
             >
                 ORDER NOW
             </button>
