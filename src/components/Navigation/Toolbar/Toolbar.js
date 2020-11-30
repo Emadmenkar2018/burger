cff05f16416ffef50209958ce121d14a0c8c253a
omar-T <ot.themechanic95@gmail.com>
2020-11-30 20:23:58 +0300

    Add Toolbar component structure and styles.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import classes from './Toolbar.module.css'
 
 const Toolbar = (props) => (
-    <header>
-        
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
     </header>
 )
 
