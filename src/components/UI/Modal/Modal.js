bafa05ae4b2aa61dfe64ec7059ff801a61239974
omar-T <ot.themechanic95@gmail.com>
2020-12-01 22:16:39 +0300

    Add the chlidren props check to shouldComponentUpdate so that the spinner is shown.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,7 +4,8 @@ import Backdrop from '../Backdrop/Backdrop'
 
 class Modal extends Component {
     shouldComponentUpdate(nextProps, nextState){
-        return nextProps.show !== this.props.show
+        const {show, children} = this.props;
+        return nextProps.show !== show || nextProps.children !== children;
     }
 
     render(){
