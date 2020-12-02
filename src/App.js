def8b6d3324aff93ec518d5a7160e5181505a991
omar-T <ot.themechanic95@gmail.com>
2020-12-02 18:25:35 +0300

    Refactor.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
-function App() {
-  return (
-    <Layout>
-      <BurgerBuilder/>
-    </Layout>
-  );
+const App = () => {
+	return (
+		<Layout>
+			<BurgerBuilder/>
+		</Layout>
+	);
 }
 
 export default App;
