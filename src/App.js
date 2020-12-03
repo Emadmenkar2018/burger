2f94b76e3f9e335fc9a831ed0ebc92f840d996cb
omar-T <ot.themechanic95@gmail.com>
2020-12-03 23:10:01 +0300

    Add switch and routes for burger builder and checkout.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+import {Route, Switch} from 'react-router-dom'
 
 const App = () => {
 	return (
 		<Layout>
-			<BurgerBuilder/>
+			<Switch>
+				<Route path='/checkout' component={Checkout}/>
+				<Route exact path='/' component={BurgerBuilder}/>
+			</Switch>
 		</Layout>
 	);
 }
