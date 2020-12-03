367b15b411ec4a60d6bc7013b88e11ec91fec79d
omar-T <ot.themechanic95@gmail.com>
2020-12-03 19:55:50 +0300

    Refactor styles.

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,8 +8,7 @@ const CheckoutSummary = (props) => {
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
             <div style={{
-                width: '300px', 
-                height: '300px', 
+                width: '100%',
                 margin: 'auto'
             }}>
                 <Burger ingredients={props.ingredients}/>
