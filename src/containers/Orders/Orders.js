44ca6c2b7d9c46dcc37d3a230fec16c7261b11c1
omar-T <ot.themechanic95@gmail.com>
2020-12-04 17:32:01 +0300

    Add Orders container.

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from '../../axios-orders'
+import Order from '../../components/Order/Order'
+
+class Orders extends Component {
+    componentDidMount(){
+
+    }
+
+    render() { 
+        return (
+            <div>
+                <Order/>
+                <Order/>
+            </div>
+        );
+    }
+}
+
+export default Orders;
