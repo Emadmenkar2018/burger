1560d2c0a1059fae1c785e65ef6f25a5469b9df3
omar-T <ot.themechanic95@gmail.com>
2020-12-04 17:32:14 +0300

    Add route for orders page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
-import {Route, Switch} from 'react-router-dom'
+import Orders from './containers/Orders/Orders'
 
 const App = () => {
 	return (
 		<Layout>
 			<Switch>
 				<Route path='/checkout' component={Checkout}/>
+				<Route path='/orders' component={Orders}/>
 				<Route exact path='/' component={BurgerBuilder}/>
 			</Switch>
 		</Layout>
