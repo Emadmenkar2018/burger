7c5a4d14628e087101d4f8486850241fad32e932
omar-T <ot.themechanic95@gmail.com>
2020-12-04 18:17:16 +0300

    Refactor to print the order ingredients and price.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,26 @@ import React from 'react';
 import classes from './Order.module.css'
 
 const Order = (props) => {
+    const ingredients = [];
+    for(let ingredientName in props.ingredients){
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+    }
+
+    const ingredientOutput = ingredients.map((ig, index) => 
+        <span 
+            key={`${ig.name}-${index}`}
+            className={classes.Ingredient}
+        >
+            {ig.name} ({ig.amount})
+        </span>
+    )
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>USD 5.45</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
         </div>
     );
 };
