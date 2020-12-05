c0a089e2b4388c4620ef7b7e8d426d09296681aa
omar-T <ot.themechanic95@gmail.com>
2020-12-05 19:21:46 +0300

    Refactor. Adjust the component to use the props from the store.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom'
+import {connect} from 'react-redux'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
-    state = this.myInitializeStateMethod();
-    
-    myInitializeStateMethod() {
-        const query = new URLSearchParams(this.props.location.search);
-        let ingredients = {};
-        let price = 0;
-        for(let param of query.entries()){
-            
-            if (param[0] === 'price'){
-                price = param[1];
-            }else {
-                ingredients[param[0]] = +param[1];
-            }
-        }
-        return {ingredients, totalPrice: price};
-    }
-
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
@@ -30,27 +14,25 @@ class Checkout extends Component {
     }
 
     render() {
-        const {ingredients, totalPrice} = this.state;
+        const {ings} = this.props;
         return (
             <div>
                 <CheckoutSummary 
-                    ingredients={ingredients}
+                    ingredients={ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                 />
                 <Route 
-                    path={`${this.props.match.url}/contact-data`} 
-                    render={(props) => 
-                        <ContactData 
-                            {...props}
-                            ingredients={ingredients}
-                            price={totalPrice}
-                        />
-                    }
+                    path={`${this.props.match.url}/contact-data`}
+                    component={ContactData}
                 />
             </div>
         );
     }
 }
 
-export default Checkout;
+const mapStateToProps = state => ({
+    ings: state.ingredients
+})
+
+export default connect(mapStateToProps)(Checkout);
