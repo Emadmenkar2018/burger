95924f0cdde20764c1966f2371c922924718ff36
omar-T <ot.themechanic95@gmail.com>
2020-12-05 18:43:13 +0300

    Add the logic for adding and removing an ingredient.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,12 @@
 import {ADD_INGREDIENTS, REMOVE_INGREDIENTS} from './actions'
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 }
 
@@ -9,11 +14,19 @@ export default (state = initialState, action) => {
     switch(action.type){
         case ADD_INGREDIENTS:
             return {
-                ...state
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
             }
         case REMOVE_INGREDIENTS:
             return {
-                ...state
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
             }
         default:
             return state;
