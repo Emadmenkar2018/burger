bfa665fb311c0b1cb993428c278d45766fd4b66a
omar-T <ot.themechanic95@gmail.com>
2020-12-06 20:08:36 +0300

    Add action creators for purchasing burger.

---
+++ src/store/actions/order.js
@@ -0,0 +1,32 @@
+import {PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAIL, PURCHASE_BURGER_START} from './actionTypes'
+import axios from '../../axios-orders'
+
+export const purchaseBurgerSuccess = (id, orderData) => ({
+    type: PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData
+})
+
+export const purchaseBurgerFail = (error) => ({
+    type: PURCHASE_BURGER_FAIL,
+    error
+})
+
+export const purchaseBurgerStart = () => ({
+    type: PURCHASE_BURGER_START
+})
+
+export const purchaseBurger = (orderData) => {
+    return dispatch => {
+        dispatch(purchaseBurgerStart());
+        axios.post('/orders.json', orderData)
+            .then(res => {
+                console.log(res.data);
+                dispatch(purchaseBurgerSuccess(res.data, orderData));
+            })
+            .catch(err => {
+                // console.log(err.response);
+                dispatch(purchaseBurgerFail(err));
+            })
+    }
+}
