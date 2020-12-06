d43f8be1c332f3b07f3d41f2a18a091be72c0815
omar-T <ot.themechanic95@gmail.com>
2020-12-06 18:19:12 +0300

    Add redux devtools.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
-import rootReducer from './store/reducer'
+import rootReducer from './store/reducers/reducer'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
+);
 
 ReactDOM.render(
     <Provider store={store}>
