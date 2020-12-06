db43f0a00e34e0030d9d76caed524b5e08fc6cf6
omar-T <ot.themechanic95@gmail.com>
2020-12-06 19:07:34 +0300

    Add thunk middleware to the store.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './store/reducers/burgerBuilder'
 
-const store = createStore(rootReducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(
     <Provider store={store}>
