c5262a7a794c91a482edc6af39a735eccd693a3b
omar-T <ot.themechanic95@gmail.com>
2020-12-06 20:40:28 +0300

    Add purchased to initial state. Add purchase  init reducer case. Refactor.

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,12 +1,23 @@
-import {PURCHASE_BURGER_SUCCESS, PURCHASE_BURGER_FAIL, PURCHASE_BURGER_START} from '../actions/actionTypes'
+import {
+    PURCHASE_BURGER_SUCCESS, 
+    PURCHASE_BURGER_FAIL, 
+    PURCHASE_BURGER_START, 
+    PURCHASE_INIT
+} from '../actions/actionTypes'
 
 const initialState = {
     orders: [],
-    loading: false
+    loading: false,
+    purchased: false
 }
 
 export default (state = initialState, action) => {
     switch(action.type){
+        case PURCHASE_INIT:
+            return {
+                ...state,
+                purchased: false
+            }
         case PURCHASE_BURGER_START:
             return {
                 ...state,
@@ -20,6 +31,7 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 loading: false,
+                purchased: true,
                 orders: state.orders.concat(newOrder)
             }
         case PURCHASE_BURGER_FAIL:
