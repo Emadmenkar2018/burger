998291a8bfac3fe417c61c6b545061eb69f09ca7
omar-T <ot.themechanic95@gmail.com>
2020-12-06 19:18:30 +0300

    Add setIngredients and fetchingredientsFailed action creators. Add initIngredients to get the ingredients from the database asynchronously.

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
-import {ADD_INGREDIENTS, REMOVE_INGREDIENTS} from './actionTypes'
+import {ADD_INGREDIENTS, REMOVE_INGREDIENTS, SET_INGREDIENTS, FETCH_INGREDIENTS_FAILED} from './actionTypes'
+import axios from '../../axios-orders'
 
 export const addIngredient = (name) => ({
     type: ADD_INGREDIENTS,
@@ -9,3 +10,25 @@ export const removeIngredient = (name) => ({
     type: REMOVE_INGREDIENTS,
     ingredientName: name
 })
+
+export const setIngredients = (ingredients) => ({
+    type: SET_INGREDIENTS,
+    ingredients
+})
+
+export const fetchIngredientsFailed = () => ({
+    type: FETCH_INGREDIENTS_FAILED
+})
+
+export const initIngredients = () => {
+    return dispatch => {
+        axios.get('/ingredients.json')
+            .then(res => {
+                // console.log(res);
+                dispatch(setIngredients(res.data));
+            })
+            .catch(err => {
+                dispatch(fetchIngredientsFailed());
+            })
+    }
+}
