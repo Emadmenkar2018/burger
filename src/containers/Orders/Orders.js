2add6dda8d8fa7618baa16d3b284d83b4dc6cd38
omar-T <ot.themechanic95@gmail.com>
2020-12-07 19:42:52 +0300

    Pass token to onFetchOrders.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,8 +8,8 @@ import * as actions from '../../store/actions'
 
 class Orders extends Component {
     componentDidMount(){
-        const {onFetchOrders} = this.props;
-        onFetchOrders();
+        const {token, onFetchOrders} = this.props;
+        onFetchOrders(token);
     }
 
     render() {
@@ -34,11 +34,12 @@ class Orders extends Component {
 
 const mapStateToProps = state => ({
     orders: state.order.orders,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
 })
 
 const mapDispatchToProps = dispatch => ({
-    onFetchOrders: () => dispatch(actions.fetchOrders())
+    onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
