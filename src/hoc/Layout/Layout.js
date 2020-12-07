892f8e9864e7a65298af7021a7f4c8db1716a687
omar-T <ot.themechanic95@gmail.com>
2020-12-07 20:01:55 +0300

    Connect the component to the store. Define mapStateToProps to get isAuthenticated value based on token value. Pass isAuth to the Toolbar and sideDrawer components.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import classes from './Layout.module.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+import { connect } from 'react-redux'
 
 class Layout extends Component {
     state = {
@@ -22,12 +23,14 @@ class Layout extends Component {
 
     render(){
         const {showSideDrawer} = this.state;
+        const {isAuthenticated} = this.props;
         return (
             <Fragment>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} isAuth={isAuthenticated}/>
                 <SideDrawer 
                     open={showSideDrawer} 
                     closed={this.sideDrawerClosedHandler}
+                    isAuth={isAuthenticated}
                 />
                 <main className={classes.Content}>
                     {this.props.children}
@@ -37,4 +40,8 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.token !== null
+})
+
+export default connect(mapStateToProps)(Layout);
