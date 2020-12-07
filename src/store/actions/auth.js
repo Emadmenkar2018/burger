1783cdaedc5b5059141085e4f007e72d9e3cf7e4
omar-T <ot.themechanic95@gmail.com>
2020-12-07 16:58:53 +0300

    Add auth action types and creators.

---
+++ src/store/actions/auth.js
@@ -0,0 +1,23 @@
+import {AUTH_START, AUTH_SUCCESS, AUTH_FAIL} from './actionTypes'
+import axios from '../../axios-orders'
+
+export const authStart = () => ({
+    type: AUTH_START
+})
+
+export const authSuccess = authData => ({
+    type: AUTH_SUCCESS,
+    authData
+})
+
+export const authFail = error => ({
+    type: AUTH_FAIL,
+    error
+})
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authStart());
+
+    }
+}
