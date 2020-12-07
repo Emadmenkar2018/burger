fd9b1e8b9d9dbd137f6b05eca2f84918c9f9f4ff
omar-T <ot.themechanic95@gmail.com>
2020-12-07 21:46:06 +0300

    Refactor the component to class. Connect the component to the store. Call onAuthCheckState in componentDidMount.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import * as actions from './store/actions'
 
-const App = () => {
-	return (
-		<Layout>
-			<Switch>
-				<Route path='/checkout' component={Checkout}/>
-				<Route path='/orders' component={Orders}/>
-				<Route path='/auth' component={Auth}/>
-				<Route path='/logout' component={Logout}/>
-				<Route exact path='/' component={BurgerBuilder}/>
-			</Switch>
-		</Layout>
-	);
+class App extends Component {
+	componentDidMount(){
+		const {onAuthCheckState} = this.props;
+		onAuthCheckState();
+	}
+
+	render(){
+		return (
+			<Layout>
+				<Switch>
+					<Route path='/checkout' component={Checkout}/>
+					<Route path='/orders' component={Orders}/>
+					<Route path='/auth' component={Auth}/>
+					<Route path='/logout' component={Logout}/>
+					<Route exact path='/' component={BurgerBuilder}/>
+				</Switch>
+			</Layout>
+		);
+	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+	onAuthCheckState: () => dispatch(actions.authCheckState())
+})
+
+export default connect(null, mapDispatchToProps)(App);
