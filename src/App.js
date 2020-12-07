0461a80b9bac64d080acf366e77bb63dea7e49a8
omar-T <ot.themechanic95@gmail.com>
2020-12-07 22:08:03 +0300

    Add guards for routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
@@ -16,22 +16,37 @@ class App extends Component {
 	}
 
 	render(){
+		const {isAuthenticated} = this.props;
+		let routes = (
+			<Switch>
+				<Route path='/auth' component={Auth}/>
+				<Route exact path='/' component={BurgerBuilder}/>
+				<Redirect to='/'/>
+			</Switch>
+		)
+		if(isAuthenticated) routes = (
+			<Switch>
+				<Route path='/checkout' component={Checkout}/>
+				<Route path='/orders' component={Orders}/>
+				<Route path='/logout' component={Logout}/>
+				<Route exact path='/' component={BurgerBuilder}/>
+				<Redirect to='/'/>
+			</Switch>
+		)
 		return (
 			<Layout>
-				<Switch>
-					<Route path='/checkout' component={Checkout}/>
-					<Route path='/orders' component={Orders}/>
-					<Route path='/auth' component={Auth}/>
-					<Route path='/logout' component={Logout}/>
-					<Route exact path='/' component={BurgerBuilder}/>
-				</Switch>
+				{routes}
 			</Layout>
 		);
 	}
 }
 
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.token !== null
+})
+
 const mapDispatchToProps = dispatch => ({
 	onAuthCheckState: () => dispatch(actions.authCheckState())
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
