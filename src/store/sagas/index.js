2089ba314807a39abe38858b41ba6a5e50277c86
omar-T <ot.themechanic95@gmail.com>
2020-12-10 22:12:26 +0300

    Add watchOrder saga watcher. Hook purchaseburgerSaga to the watcher.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -7,14 +7,19 @@ import {
     authCheckStateSaga
 } from './auth'
 import {initIngredientsSaga} from './burgerBuilder'
+import {purchaseBurgerSaga} from './order'
 
-export function* watchAuth(){
+export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
 
-export function* watchBurgerBuilder(){
+export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga)
 }
