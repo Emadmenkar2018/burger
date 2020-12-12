8a88c6ca6369f256946ab46573d447fe6a43b1a5
omar-T <ot.themechanic95@gmail.com>
2020-12-12 17:10:49 +0300

    Refactor to use the custom hook http error handler.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,35 +1,16 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import Modal from '../../components/UI/Modal/Modal'
+import useHttpErrorHandler from '../../hooks/http-error-handler'
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return (props) => {
-        const [error, setError] = useState(null);
+        const [error, clearError] = useHttpErrorHandler(axios);
 
-        const reqInterceptor = axios.interceptors.request.use(req => {
-            setError(null);
-            return req;
-        })
-        const resInterceptor = axios.interceptors.response.use(res => res, error => {
-            setError(error);
-        })
-
-        useEffect(() => {
-            return () => {
-                // console.log('will unmount', reqInterceptor, resInterceptor);
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.response.eject(resInterceptor);
-            };
-        },[reqInterceptor, resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
-        
         return (
             <Fragment>
                 <Modal 
                     show={error}
-                    modalClosed={errorConfirmedHandler}
+                    modalClosed={clearError}
                 >
                     {error ? error.message : null}
                 </Modal>
